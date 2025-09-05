c12e0d6799f2624a1b888999a4fce5b77ed9a35c
Andriw Fonseca <andriw.fonseca@totvs.com.br>
2025-09-04 21:38:46 -0300

    feat: added level change

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,12 @@ extends CharacterBody2D
 # $AnimatedSprite2D é o nome do componente na cena ($ + o nome)
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
+@export var next_level_name: String
+
+@export var max_level_limit_x: int = 0
+@export var min_level_limit_x: int = 5
+@export var level_limit_y: int = 208
+
 # Variáveis de movimento
 const SPEED = 80.0
 const JUMP_VELOCITY = -300.0
@@ -20,10 +26,7 @@ func _physics_process(delta):
 
 	# Detectar movimento horizontal
 	var direction = Input.get_axis("left", "right")
-	if direction != 0:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	move_player(direction)
 
 	# animação
 	if is_on_floor():
@@ -41,3 +44,23 @@ func _physics_process(delta):
 
 	# Aplicar o movimento
 	move_and_slide()
+
+	# ir para o próximo nível
+	if position.x > max_level_limit_x:
+		go_to_next_level()
+
+func move_player(direction: int) -> void:
+	if direction > 0 and position.x < max_level_limit_x + 10:
+		velocity.x = direction * SPEED
+	elif direction < 0 and position.x > min_level_limit_x:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+func go_to_next_level() -> void:
+	if next_level_name == "":
+		push_error("Next level name is empty")
+		return
+
+	var path_next_level = "res://scenes/" + next_level_name + ".tscn"
+	get_tree().change_scene_to_file(path_next_level)
